27be5945343761e848e32f417faff6f55bb94750
arthurohlweiler <arthur.ohlweiler@aluno.senai.br>
2026-07-20 21:11:10 -0300

    reforço

---
+++ reforco/atividade02.py
@@ -0,0 +1,8 @@
+#Entrada : pedimos o nome do usuário
+nome = input("digite seu nome ")
+
+#processamento: criamos amensagem
+mesagem = "olá "+ nome +"! bem-vindo ao phython!"
+
+#saída: exbimos a mensagem
+print(mesagem)
---
+++ reforco/atividade03.py
@@ -0,0 +1,4 @@
+numero = input ("escreva um numero")
+#para tranformar em texto é necessario int
+dobro = int(numero) * 2
+print(f"o dobro de  {numero}  é  {dobro}" )
